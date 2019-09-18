095b9f61e24841ff67f77eeae38cc00eb8e64ce8
Luz <zulsb2093@gmail.com>
2019-09-18 22:37:23 +0000

    Size of various types on the compute

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of a char:%lu 1 byte(s)\n", sizeof(char));
-	printf("Size of an int:%lu 4 byte(s)\n", sizeof(int));
-	printf("Size of a long int:%lu 4 byte(s)\n", sizeof(long));
-	printf("Size of a long long int:%lu 8 byte(s)\n", sizeof(double));
-	printf("Size of a float:%lu 4 byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(double));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
