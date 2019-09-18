8db54eda61f3daaf3367a8da382e51b93dfee16b
Luz <zulsb2093@gmail.com>
2019-09-18 21:55:20 +0000

    Size of various types on the compute

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof(char)); 
+	printf("%lu\n", sizeof(int)); 
+	printf("%lu\n", sizeof(float)); 
+	printf("%lu", sizeof(double));
+	return (0);
+}
