77ec4198e9af9541ce46d4cadd7c3976906a92d9
Luz <zulsb2093@gmail.com>
2019-09-20 01:54:29 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+	putchar(number);
+	number++;
+	}
+	putchar('\n');
+	return (0);
+}
