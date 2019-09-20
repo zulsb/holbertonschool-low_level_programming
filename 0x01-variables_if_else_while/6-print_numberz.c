266839b49026b0cd94fc3207766c1dacd2e427b2
Luz <zulsb2093@gmail.com>
2019-09-20 01:57:58 +0000

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - entry point
- *
+ * Desc: Use `putchar` function to print number 0 to 9.
  * Return: 0
  */
 int main(void)
 {
-	int number = 0;
+	int number = '0';
 
-	while (number <= 9)
+	while (number <= '9')
 	{
 	putchar(number);
 	number++;
