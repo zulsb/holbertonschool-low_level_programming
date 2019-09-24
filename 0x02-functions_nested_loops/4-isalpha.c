15d1b937d25dcbb85e0503e3aeb8cec2e7dda1c1
Luz <zulsb2093@gmail.com>
2019-09-24 18:04:50 +0000

    Function that checks for alphabetic character uppercase and lowercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _islower - checks for lowercase and uppercase character
+ * _isalpha - checks for lowercase and uppercase character
  *@c: iam parameter.
  * Return: Always 0 (Success)
  */
