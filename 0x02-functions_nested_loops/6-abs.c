b30e5b95ec4996f58d8a4384e52bcd743fd20f00
Luz <zulsb2093@gmail.com>
2019-09-24 19:40:48 +0000

    Absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _abs - Absolute value of an integer
+ * @r: Parameter
+ * Return: Always 0 (Success)
+ */
+int _abs(int r)
+{
+	if (n >= 0)
+	return (n);
+	return (0 * -1);
+}
+
