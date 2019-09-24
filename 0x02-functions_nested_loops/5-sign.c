1eca5b21fe066e778fae338258f6ecc02e1e0cd6
Luz <zulsb2093@gmail.com>
2019-09-24 18:38:19 +0000

    Sing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	_putchar ('+');
+	{
+	putchar ('+');
 	return (1);
+	}
 	else if (n == 0)
-	_putchar ('0');
+	{
+	putchar ('0');
 	return (0);
+	}
 	else
+	{
 	putchar ('-');
 	return (-1);
+	}
 }
