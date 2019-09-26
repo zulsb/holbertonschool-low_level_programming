a15a7e651933e51ec431f4f4accc35d1588367d2
Luz <zulsb2093@gmail.com>
2019-09-26 15:39:24 +0000

    Numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Print numbers 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	char x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+}
