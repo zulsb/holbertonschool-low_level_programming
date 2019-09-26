8c199396449a52cef89d1820adc839cd5987a6d7
Luz <zulsb2093@gmail.com>
2019-09-26 20:11:33 +0000

    Line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_line - Print line..
+ *@n: i'm parameter.
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+		_putchar(95);
+		_putchar('\n');
+}
+
