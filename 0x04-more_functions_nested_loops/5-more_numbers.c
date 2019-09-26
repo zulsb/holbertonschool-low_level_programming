7fbb903f9d8f586f19baf202bcf0d1159a141def
Luz <zulsb2093@gmail.com>
2019-09-26 19:21:46 +0000

    Numbers from 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,8 @@ void more_numbers(void)
 	{
 	for (x = 0; x <= 14; x++)
 	{
-	if ((x / 10) != '0')
-	_putchar((x / 10) + '0');
+		if ((x / 10) != 0)
+		_putchar((x / 10) + '0');
 	_putchar((x % 10) + '0');
 	}
 	_putchar('\n');
