ff066b53399b7e558726e0fae6dcf9ba475b7292
Luz <zulsb2093@gmail.com>
2019-09-26 19:07:14 +0000

    Numbers from 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Prints numbers 0 to 14, 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (y = 0; y < 10; y++)
+	{
+	for (x = 0; x <= 14; x++)
+	{
+	if ((x / 10) != '0')
+	_putchar((x / 10) + '0');
+	_putchar((x % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
