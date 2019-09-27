dde2a37bcaa431a444a3dddc372c247d7548522f
Luz <zulsb2093@gmail.com>
2019-09-27 00:47:48 +0000

    Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_square - Print square..
+ *@size: i'm parameter.
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (i == 0)
+		_putchar('\n');
+}
