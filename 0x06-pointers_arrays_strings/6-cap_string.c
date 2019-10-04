dc5b21b7639b0519f902dc2ff5a0b69eb33cd572
Luz <zulsb2093@gmail.com>
2019-10-04 02:51:14 +0000

    Capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+  *cap_string - Capitalizes all words of a string
+  *@s: pointer
+  *Return: string concatenate.
+  */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char t[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}', '\0'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (t[j] != '\0')
+		{
+			if (s[i] == t[j])
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] = s[i + 1] - 32;
+				j++;
+		}
+	}
+
+	return (s);
+}
