e0872a86e57f50eaf8a0929cdece823fd7323de2
Luz <zulsb2093@gmail.com>
2019-10-10 23:51:00 +0000

    Prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+  *is_prime_number - Print prime number.
+  *@n: varible.
+  *Return: 1 or 0.
+  */
+
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
+}
+
+/**
+  *prime - Print prime number
+  *@n: variable
+  *@x: variable.
+  *Return: 1 or 0.
+  */
+
+int prime(int n, int x)
+{
+
+	if (x > n / 2)
+		return (1);
+	else if (n % x == 0 || n < 2)
+		return (0);
+
+	return (prime(n, x + 1));
+
+}
