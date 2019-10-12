90b73ee6fe715b3157d6070d7c075b67a3e042ca
Luz <zulsb2093@gmail.com>
2019-10-12 23:03:22 +0000

    Whats my name!

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *main - Program name
+  *@argc: Argument count
+  *@argv: Argument value
+  *Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("s%\n", argv[0]);
+		return (0);
+}
