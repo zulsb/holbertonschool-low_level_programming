97b4bed2f467e014e258f32f35c91e288734e261
Luz <zulsb2093@gmail.com>
2019-10-12 23:43:58 +0000

    Whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 
 /**
   *main - Program name
-  *@argc: Argument count
+  *@argc: Argument count int
   *@argv: Argument value
   *Return: 0
   */
 
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf("s%\n", argv[0]);
+	printf("%s\n", argv[0]);
 		return (0);
 }
