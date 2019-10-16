717267e4faa508186255b1a50e8f52749047c728
Luz <zulsb2093@gmail.com>
2019-10-16 02:07:49 +0000

    Pointer to a 2 dimensional array of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  *alloc_grid - Pointer to a 2 dimensional array of integers.
+  *@with: variable.
+  *@height: variable int.
+  *Return: NULL.
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int c, s;
+	int **a;
+
+	for (c = 0; c < height; c++)
+	{
+
+	}
+	
+
+}
+
