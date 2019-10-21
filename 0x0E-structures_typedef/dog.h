760c5c44050013ed066a507e912cc1d80461d628
Luz <zulsb2093@gmail.com>
2019-10-21 22:59:09 +0000

    Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _mydog
+#define _mydog
+#include <stdio.h>
+
+
+/**
+  *struct dog - Structures
+  *@name: pointer.
+  *@age: varible float.
+  *@owner: pointer.
+  *Description: dogs information.
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
