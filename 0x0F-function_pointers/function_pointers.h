9c75d33bff7fdbe44b8272f9c04610045b6df5ae
Luz <zulsb2093@gmail.com>
2019-10-23 19:41:24 +0000

    Prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _FUNCTIONS
 #define _FUNCTIONS
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
