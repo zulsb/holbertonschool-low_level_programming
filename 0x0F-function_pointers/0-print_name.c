4e52f21d76994d6c66f5be25660ce15765ab97dd
Luz <zulsb2093@gmail.com>
2019-10-23 18:18:31 +0000

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+  *print_name - Function that prints a name.
+  *@name: pointer char.
+  *@f: pointer.
+  *
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == '\0' || name == '\0')
+		f(name);
+
+}
