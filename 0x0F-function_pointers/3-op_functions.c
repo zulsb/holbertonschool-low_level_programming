613dd1c013828c591b129813a08517e4cbd4a81a
Luz <zulsb2093@gmail.com>
2019-10-24 00:58:31 +0000

    fix2

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+/**
+  *op_add - Sum
+  *@a: first variable
+  *@b: second variable
+  *Return: sum.
+  */
+int op_add(int a, int b)
+{
+int sum;
+
+sum = a + b;
+return (sum);
+}
+/**
+  *op_sub - rest
+  *@a: first variable
+  *@b: second variable
+  *Return: rest.
+  */
+int op_sub(int a, int b)
+{
+int rest;
+
+rest = a - b;
+return (rest);
+}
+/**
+  *op_mul - Mul
+  *@a: first variable
+  *@b: second variable
+  *Return: mul.
+  */
+int op_mul(int a, int b)
+{
+int mul;
+
+mul = a * b;
+return (mul);
+}
+/**
+  *op_div - div
+  *@a: first variable
+  *@b: second variable
+  *Return: div.
+  */
+int op_div(int a, int b)
+{
+int div;
+
+div = a / b;
+return (div);
+}
+/**
+  *op_mod - mod
+  *@a: first variable
+  *@b: second variable
+  *Return: mod.
+  */
+int op_mod(int a, int b)
+{
+int mod;
+
+mod = a % b;
+return (mod);
+}
