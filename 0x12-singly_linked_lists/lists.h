a4b6520f8c8d3b0d975c44e74b05cad1c044047b
Luz <zulsb2093@gmail.com>
2019-10-31 21:12:06 +0000

    Prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _LINKED
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
