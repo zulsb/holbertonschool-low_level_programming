5ee534e56524225b7a92ba5148224465938a515d
Luz <zulsb2093@gmail.com>
2019-10-31 21:59:29 +0000

    Elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,14 +8,14 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t numm;
+	size_t num_e;
 
-	numm = 0;
+	num_e = 0;
 
-	if (h != '\0')
+	while (h != '\0')
 	{
 		h = h->next;
-		numm++;
+		num_e++;
 	}
-	return (numm);
+	return (num_e);
 }
