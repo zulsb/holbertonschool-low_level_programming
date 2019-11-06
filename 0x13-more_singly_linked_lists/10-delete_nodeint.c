ac903a760388a328bf41541e596372ce80663071
Luz <zulsb2093@gmail.com>
2019-11-06 03:03:24 +0000

    Dete Node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(rec);
 		return (1);
 	}
-	for (s = 1; s < index; s++)
+	for (s = 0; rec != '\0' && s < (index - 1); s++)
 	{
 		rec = (*rec).next;
 	}
