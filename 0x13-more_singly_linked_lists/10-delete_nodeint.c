6a34179e5da6a3cbea91518996865a74687dd101
Luz <zulsb2093@gmail.com>
2019-11-06 03:23:46 +0000

    Dete Node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,7 +7,7 @@
   */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *rec, *next;
+	listint_t *rec, *n;
 	unsigned int s;
 
 	if (*head == '\0')
@@ -26,9 +26,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (rec == '\0' || (*rec).next == '\0')
 		return (-1);
 
-	next = rec;
-	next = rec->next->next;
+	n = rec;
+	n = rec->next->next;
 	free((*rec).next);
-	(*rec).next = next;
+	(*rec).next = n;
 	return (1);
 }
