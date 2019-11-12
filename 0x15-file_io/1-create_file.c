5abf545a4b33972a76d4aff5a7073963c38939dc
Luz <zulsb2093@gmail.com>
2019-11-12 22:13:30 +0000

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	char arc, acc;
 
 	acc = 0;
-	arc = open(filename, O_CREAT | O_WRONLY, 0600);
+	arc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (arc == -1)
 		return (-1);
